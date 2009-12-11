7b22e9b11e953288611363c0958666c113499481
Paul Visscher <paulv@canonical.org>
2009-12-11 00:58:38 -0500

    Initial commit of ruby FFI interface to libgpod (http://www.gtkpod.org/libgpod/).

---
+++ gpod.rb
@@ -0,0 +1,34 @@
+require 'ffi'
+
+module GPod
+  class GList < ::FFI::Struct
+    layout :data, :pointer, :next, :pointer, :prev, :pointer
+  end
+
+  class Itdb_iTunesDB < ::FFI::Struct
+    layout(
+           :tracks, :pointer,
+           :playlists, :pointer,
+           :filename, :pointer,
+           :device, :pointer,
+           :version, :int32,
+           :id, :int64,
+           :reserved_int1, :int32,
+           :reserved_int2, :int32,
+           :reserved1, :pointer,
+           :reserved2, :pointer,
+           :usertype, :uint64,
+           :userdata, :pointer,
+           :userdata_duplicate, :pointer,
+           :userdata_destroy, :pointer
+           )
+  end
+  extend FFI::Library
+  ffi_lib 'gpod'
+
+  attach_function :itdb_parse, [:string, :pointer], :pointer
+end
+
+x = GPod.itdb_parse("/tmp/ipod/", nil)
+
+puts "#{x}"
